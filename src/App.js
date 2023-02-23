61ec24356edc29742ff504e2fec4e15f7e1e5ee7
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-02-23 12:28:04 +0300

    Update useing Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/calculator';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Quotes from './components/Qoute';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-    </div>
-
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quotes />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
